fa86249b66bddb4bc0d25f9307aca64bf73dddbc
Brandon <williamomballa@gmail.com>
2025-12-05 07:41:17 +0300

    Adding meilisearch

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -5,6 +5,8 @@ import { StoreRegion } from "@medusajs/types"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import CartButton from "@modules/layout/components/cart-button"
 import SideMenu from "@modules/layout/components/side-menu"
+import SearchModal from "@modules/search/components/modal"
+
 
 export default async function Nav() {
   const regions = await listRegions().then((regions: StoreRegion[]) => regions)
@@ -28,7 +30,7 @@ export default async function Nav() {
               ExaMek
             </LocalizedClientLink>
           </div>
-
+          <SearchModal />
           <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
             <div className="hidden small:flex items-center gap-x-6 h-full">
               <LocalizedClientLink
